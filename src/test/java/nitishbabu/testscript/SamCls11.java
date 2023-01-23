0734f2f3d5ddc4597569b01902cf658b62640489
Administrator <Administrator@DESKTOP-FUGQNF4>
2023-01-23 12:38:21 +0530

    ukjj

---
+++ src/test/java/nitishbabu/testscript/SamCls11.java
@@ -0,0 +1,17 @@
+package nitishbabu.testscript;
+
+import org.testng.annotations.Test;
+
+public class SamCls11 {
+	@Test
+	public void simple1() {
+		System.out.println("--Am  Groot--");
+	}
+	@Test
+	public void simple2() {
+		System.out.println("--Am  Groot--");
+	}
+	
+	
+
+}
