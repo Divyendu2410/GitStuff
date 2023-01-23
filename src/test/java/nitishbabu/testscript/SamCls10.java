bbd44a076821a644104ffacba83c0212b9974e24
Administrator <Administrator@DESKTOP-FUGQNF4>
2023-01-23 13:01:23 +0530

    ,jbnj,khblghjkn,

---
+++ src/test/java/nitishbabu/testscript/SamCls10.java
@@ -0,0 +1,16 @@
+package nitishbabu.testscript;
+
+import org.testng.annotations.Test;
+
+public class SamCls10 {
+	@Test
+	public void simple1() {
+		System.out.println("--Am  Groot--");
+	}
+	@Test
+	public void simple2() {
+		System.out.println("--Am  Groot--");
+	}
+	
+
+}
